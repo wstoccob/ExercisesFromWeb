64ae1e0b5c97adad1fb444d9789c335ba7f3850f
wstoccob <alinur.seysekov@gmail.com>
2023-10-22 22:51:31 +0600

    2 solved

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -6,22 +6,14 @@ internal class Exercise
     {
         while (true)
         {
-            string input = Console.ReadLine();
+            int input = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(SumOfTwo(input));
+            Console.WriteLine(AbsDif(input));
         }
-        
     }
 
-    public static int SumOfTwo(string expression)
+    public static int AbsDif(int num)
     {
-        int sum, numberOne, numberTwo;
-        int num = expression.IndexOf(",");
-
-        numberOne = Convert.ToInt32(expression.Substring(0, num));
-        numberTwo = Convert.ToInt32(expression.Substring(num+2, expression.Length - num - 2));
-        
-        
-        return numberOne == numberTwo ? 3 * (numberOne + numberTwo): numberOne + numberTwo;
+        return (num > 51) ? 3 * (num - 51) : 51 - num;
     }
 }
